14c169234d1cbf3c8b97721ff8ec3efafe35ce0f
Ibrarr <ibrarr2000@gmail.com>
2025-02-01 18:52:51 +0000

    add video length

--- template-parts/standard-article-card-no-col.php
+++ template-parts/standard-article-card-no-col.php
@@ -29,5 +29,8 @@
         <p class="term"><?php echo $term_to_show; ?></p>
         <p class="title"><?php the_title(); ?></p>
         <p class="excerpt"><?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?></p>
+	    <?php if ($term_name === 'Partner Video' || $term_name === 'Podcast') { ?>
+            <p class="length"><?php echo get_post_meta( $post->ID, 'video_length', true ); ?> min</p>
+	    <?php } ?>
     </div>
 </a>
